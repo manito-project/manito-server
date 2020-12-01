e1a0494c64bc9f3dc707c267193b003b4f69ffa2
Woo-yeong Kim <kimwy1997@gmail.com>
2020-12-01 22:10:58 +0900

    Create methods for User

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -3,16 +3,121 @@ const responseMessage = require("../modules/responseMessage");
 const statusCode = require("../modules/statusCode");
 const util = require("../modules/util");
 
+// TODO: populate rooms
 module.exports = {
   signin: async (req, res) => {
-    const { nickname, serialNumber } = req.body;
+    const { username, serialNumber } = req.body;
 
+    // TODO: error handling
     try {
-      const user = await User.create({ nickname, serialNumber });
+      const user = await User.create({ username, serialNumber });
       console.log(user);
       res
         .status(statusCode.OK)
         .send(util.success(statusCode.OK, responseMessage.CREATED_USER, user));
+    } catch (error) {
+      console.log(error);
+      res
+        .status(statusCode.INTERNAL_SERVER_ERROR)
+        .send(
+          util.fail(
+            statusCode.INTERNAL_SERVER_ERROR,
+            responseMessage.INTERNAL_SERVER_ERROR,
+          ),
+        );
+    }
+  },
+
+  getAllUsers: async (req, res) => {
+    try {
+      const users = await User.findAll({
+        attributes: ["id", "username", "serialNumber"],
+      });
+      if (!users) {
+        return res
+          .status(statusCode.BAD_REQUEST)
+          .send(
+            util.fail(
+              statusCode.BAD_REQUEST,
+              responseMessage.GET_ALL_USERS_FAIL,
+            ),
+          );
+      }
+      console.log(users);
+
+      res
+        .status(statusCode.OK)
+        .send(
+          util.success(
+            statusCode.OK,
+            responseMessage.GET_ALL_USERS_SUCCESS,
+            users,
+          ),
+        );
+    } catch (error) {
+      console.log(error);
+      res
+        .status(statusCode.INTERNAL_SERVER_ERROR)
+        .send(
+          util.fail(
+            statusCode.INTERNAL_SERVER_ERROR,
+            responseMessage.INTERNAL_SERVER_ERROR,
+          ),
+        );
+    }
+  },
+
+  // TODO: user verification
+  getOneUser: async (req, res) => {
+    const { userId } = req.params;
+
+    try {
+      const user = await User.findOne({
+        where: { id: userId },
+        attributes: ["id", "username", "serialNumber"],
+      });
+      if (!user) {
+        return res
+          .status(statusCode.BAD_REQUEST)
+          .send(util.fail(statusCode.BAD_REQUEST, responseMessage.NO_USER));
+      }
+      res
+        .status(statusCode.OK)
+        .send(
+          util.success(
+            statusCode.OK,
+            responseMessage.GET_ONE_USER_SUCCESS,
+            user,
+          ),
+        );
+    } catch (error) {
+      console.log(error);
+      res
+        .status(statusCode.INTERNAL_SERVER_ERROR)
+        .send(
+          util.fail(
+            statusCode.INTERNAL_SERVER_ERROR,
+            responseMessage.INTERNAL_SERVER_ERROR,
+          ),
+        );
+    }
+  },
+  deleteUser: async (req, res) => {
+    const { userId } = req.params;
+    try {
+      const deletedUser = await User.destroy({
+        where: { id: userId },
+        attributes: ["id", "username", "serialNumber"],
+      });
+      res
+        .status(statusCode.OK)
+        .send(
+          util.success(
+            statusCode.OK,
+            responseMessage.DELETE_USER_SUCCESS,
+            deletedUser,
+          ),
+        );
     } catch (error) {
       console.log(error);
       res
