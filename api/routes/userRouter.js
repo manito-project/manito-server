ee95dcdaa622b4446a46534f9215e5a49fa743bb
Woo-yeong Kim <kimwy1997@gmail.com>
2020-12-01 22:11:36 +0900

    Connect userRouter to userController

--- api/routes/userRouter.js
+++ api/routes/userRouter.js
@@ -1,19 +1,20 @@
 const express = require("express");
+const userController = require("../controllers/userController");
 const userRouter = express.Router();
 
 // Create a user
-userRouter.post("/");
+userRouter.post("/", userController.signin);
 
 // Read all users
-userRouter.get("/");
+userRouter.get("/", userController.getAllUsers);
 
 // Read a user by ID
-userRouter.get("/:userId");
+userRouter.get("/:userId", userController.getOneUser);
 
 // Update a user
 userRouter.put("/:userId");
 
 // Delete a user
-userRouter.delete("/:userId");
+userRouter.delete("/:userId", userController.deleteUser);
 
 module.exports = userRouter;
