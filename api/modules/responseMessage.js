d7bab905b816f1c0192f1f8bc6d2ba86a3b1a910
Woo-yeong Kim <kimwy1997@gmail.com>
2020-12-04 14:42:48 +0900

    Update responseMessage

--- api/modules/responseMessage.js
+++ api/modules/responseMessage.js
@@ -3,7 +3,6 @@ module.exports = {
   OUT_OF_VALUE: "파라미터 값이 잘못되었습니다",
 
   // 회원가입
-  CREATED_USER: "회원 가입 성공",
   DELETE_USER: "회원 탈퇴 성공",
   ALREADY_ID: "이미 사용중인 아이디입니다.",
 
@@ -22,8 +21,38 @@ module.exports = {
 
   DB_ERROR: "DB 오류",
 
+  // 유저
+  CREATE_USER_SUCCESS: "유저 생성 성공",
+  CREATE_USER_FAIL: "유저 생성 실패",
+  GET_ALL_USERS_SUCCESS: "모든 유저 조회 성공",
+  GET_ALL_USERS_FAIL: "모든 유저 조회 실패",
+  GET_ONE_USER_SUCCESS: "유저 조회 성공",
+  GET_ONE_USER_FAIL: "유저 조회 실패",
+  DELETE_USER_SUCCESS: "유저 삭제 성공",
+  DELETE_USER_FAIL: "유저 삭제 실패",
+
+  // 방
+  CREATE_ROOM_SUCCESS: "방 생성 성공",
+  CREATE_ROOM_FAIL: "방 생성 실패",
+  GET_ALL_ROOMS_SUCCESS: "모든 방 조회 성공",
+  GET_ALL_ROOMS_FAIL: "모든 방 조회 실패",
+  GET_ONE_ROOM_SUCCESS: "방 조회 성공",
+  GET_ONE_ROOM_FAIL: "방 조회 실패",
+  DELETE_ROOM_SUCCESS: "방 삭제 성공",
+  DELETE_ROOM_FAIL: "방 삭제 실패",
+
+  // 방 입장
+  ROOM_ENTER_SUCCESS: "방 입장 성공",
+  INVALID_CODE: "초대코드가 잘못되었습니다",
+
+  // 마니또 매칭
+  MATCHING_SUCCESS: "매칭 성공",
+
   /* 프로필 */
-    PROFILE_GET_SUCCESS: "프로필 기본정보 조회 성공",
-    PROFILE_POST_SUCCESS: "프로필 등록 성공",
-    PROFILE_POST_FAIL : "프로필 등록 실패"
+  PROFILE_GET_SUCCESS: "프로필 기본정보 조회 성공",
+  PROFILE_POST_SUCCESS: "프로필 등록 성공",
+  PROFILE_POST_FAIL: "프로필 등록 실패",
+
+  /* 서버에러 */
+  INTERNAL_SERVER_ERROR: "서버 내부 오류",
 };
